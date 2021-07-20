133d0de02528f9dc0870f8f7af0ff7e914ac1ffc
Praharsh Suryadevara <praharsharmm@gmail.com>
2021-07-20 10:05:54 -0500

    Bug fixes, helper function to group data generation

--- src/utils/global_folder_vars.py
+++ src/utils/global_folder_vars.py
@@ -3,5 +3,5 @@ Stores locations of global folder variables.
 """
 
 
-# folder containing all the data
+# folder containing all the data. Ideally this should be translated to a local variable before each run.
 BASE_DIRECTORY = '/home/praharsh/Dropbox/research/bv-libraries/basinerror/datainv'
--- src/utils/params.py
+++ src/utils/params.py
@@ -9,9 +9,6 @@ from global_folder_vars import BASE_DIRECTORY
 import re
 
 
-
-
-
 class SystemParamHSWCA(Enum):
     """ Parameters that uniquely define a system along with
         a start coordinate set for minimization. As done for HS_WCA
@@ -144,21 +141,28 @@ def generate_save_run_params_ip_binary(par, folder, seed=0):
     initial_coords = np.random.rand(
         par.n_part.value * par.ndim.value) * box_length
     print(initial_coords, 'initial coords')
-    print(get_ncellsx_scale(hs_radii, [box_length, box_length, box_length]), 'cell scale')
+    print(get_ncellsx_scale(
+        hs_radii, [box_length, box_length, box_length]), 'cell scale')
     print(len(initial_coords), "initial_coords length")
     os.makedirs(folder + '/' + 'sec_params' + '/' + str(seed), exist_ok=True)
-    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) + '/hs_radii.txt', hs_radii, delimiter=',')
-    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) + '/initial_coords.txt', initial_coords, delimiter=',')
-    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) + '/box_length.txt', [box_length], delimiter=',')
+    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) +
+               '/hs_radii.txt', hs_radii, delimiter=',')
+    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) +
+               '/initial_coords.txt', initial_coords, delimiter=',')
+    np.savetxt(folder + '/' + 'sec_params' + '/' + str(seed) +
+               '/box_length.txt', [box_length], delimiter=',')
+
 
 def load_secondary_params_ip_binary(folder, seed=0):
     """Loads secondary parameters, should write this into a class
     """
 
-
-    hs_radii = np.loadtxt(folder + '/' + 'sec_params' + '/' + str(seed)  + '/hs_radii.txt', delimiter=',')
-    initial_coords = np.loadtxt(folder+ '/' + 'sec_params' + '/' + str(seed)  + '/initial_coords.txt', delimiter=',')
-    box_length = np.loadtxt(folder + '/' + 'sec_params' + '/' + str(seed) + '/box_length.txt', delimiter=',')
+    hs_radii = np.loadtxt(folder + '/' + 'sec_params' +
+                          '/' + str(seed) + '/hs_radii.txt', delimiter=',')
+    initial_coords = np.loadtxt(
+        folder + '/' + 'sec_params' + '/' + str(seed) + '/initial_coords.txt', delimiter=',')
+    box_length = np.loadtxt(folder + '/' + 'sec_params' +
+                            '/' + str(seed) + '/box_length.txt', delimiter=',')
     return hs_radii, initial_coords, float(box_length)
 
 
@@ -187,7 +191,8 @@ def make_system_folder(param, datadir):
     os.makedirs(foldername, exist_ok=True)
     return foldername
 
-def get_system_parameters_from_folder(folder):
+
+def get_system_parameters_from_folder(foldname):
     """ Loads system parameters from the folder string
 
     Parameters
@@ -199,42 +204,41 @@ def get_system_parameters_from_folder(folder):
     SystemParam
         SystemParam defining the system
     """
-    foldname = "ndim=2phi=0.9seed=0n_part=16r1=1.0r2=1.4rstd1=0.05rstd2=0.06999999999999999use_cell_lists=0power=2.5eps=1.0"
     # split by floatin point number
     split_foldname = re.split(r"=([-+]?\d*\.\d+|\d+)", foldname)
     split_foldname = split_foldname[:-1]
     param_dict = {}
     for i in range(len(split_foldname)):
-         if i % 2 == 0:
-             param_dict[split_foldname[i]] = float(split_foldname[i + 1])
+        if i % 2 == 0:
+            param_dict[split_foldname[i]] = float(split_foldname[i + 1])
     print(param_dict)
     return Enum('SystemParam', param_dict)
 
 
+def generate_save_all_params_ip_binary(param, datadir, seed=0):
+    """ Generates and saves both primary and secondary parameters
+        Creates a new folder called sec_params if it does not exist and a subfolder defined by the seed
+
+    Parameters
+    ----------
+    par : SystemParamInversePowerBinary
+        System defining the parameters
+    datadir : str
+        Location of the data folder
+    seed : int, optional
+        seed for generating random parameters
+    """
+    folder = make_system_folder(param, datadir=datadir)
+    # necessary for legacy compatibility
+    save_params(param, folder)
+    generate_save_run_params_ip_binary(param, folder, seed)     
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     # save system parameters in a unique directory
     datadir = BASE_DIRECTORY
     os.makedirs(datadir, exist_ok=True)
-    param = SystemParamInversePowerBinary
-    namestr = ''
-    paramdict = {}
-    for name, member in param.__members__.items():
-        namestr += name
-        namestr += "="
-        namestr += str(member.value)
-        paramdict[name] = member.value
-    print("unique directory name " + namestr)
-    # make the directory
-    foldername = datadir + '/' + namestr
-    os.makedirs(foldername, exist_ok=True)
-    
-    # Inverse power saving
-    save_params(param, foldername)
-    generate_save_run_params_ip_binary(param, foldername)
-    # load the parameters
-    th = get_system_parameters_from_folder(foldername)
-    print(th)
-    print(th.ndim.value, th.phi.value, th.seed.value, th.n_part.value, th.r1.value, th.r2.value, th.rstd1.value, th.rstd2.value, th.use_cell_lists.value, th.power.value, th.eps.value)
+    Param = SystemParamInversePowerBinary
+    generate_save_all_params_ip_binary(Param, datadir)
+
+
